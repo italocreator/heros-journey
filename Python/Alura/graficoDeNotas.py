47f1ef081c63b40d98e3f3bd7c13416a285fa836
Italo <contato.italoandrade@gmail.com>
2021-10-06 11:37:15 -0300

    add pyplot

---
+++ Python/Alura/graficoDeNotas.py
@@ -0,0 +1,17 @@
+# grafico de notas
+import matplotlib.pyplot
+from random import randrange
+import matplotlib.pyplot as plt
+
+notas_matematica = []
+
+for notas in range(8):
+    notas_matematica.append(randrange(0, 11))
+
+x = list(range(1, 9))
+y = notas_matematica
+plt.plot(x, y, marker='o')
+plt.title('Notas de matemática')
+plt.xlabel('Provas')
+plt.ylabel('Notas')
+plt.show()
