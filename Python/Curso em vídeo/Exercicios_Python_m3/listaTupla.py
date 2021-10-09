28f7160e92d1410a655dbb8e25b8bfef31061956
italocreator <contato.italoandrade@gmail.com>
2021-10-08 21:59:13 -0300

    Create listaTupla.py

---
+++ Python/Curso em vídeo/Exercicios_Python_m3/listaTupla.py
@@ -0,0 +1,16 @@
+lista_escolar = ('lapis', 1.75,
+                 'borracha', 2,
+                 'estojo', 15.90,
+                 'transferidor', 4.20,
+                 'compasso', 9.99,
+                 'mochila', 120.32,
+                 'cametas', 22.30,
+                 'livro', 34.90)
+print('-' * 30)
+print('LISTAGEM DE PREÇOS')
+print('-' * 30)
+for pos in range(0, len(lista_escolar)):
+    if pos % 2 == 0:
+        print(f'{lista_escolar[pos]:.<30}', end='')
+    else:
+        print(f'R${lista_escolar[pos]:>7.2f}')
