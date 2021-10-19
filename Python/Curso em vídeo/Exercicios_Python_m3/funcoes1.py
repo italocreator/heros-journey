9aad407c1fc640f08cd65e1d505c5fdbde66eefa
italocreator <contato.italoandrade@gmail.com>
2021-10-19 18:23:18 -0300

    Create funcoes1.py

---
+++ Python/Curso em vídeo/Exercicios_Python_m3/funcoes1.py
@@ -0,0 +1,20 @@
+# def area(l, c):
+#     a = l * c
+#     print(f'A área de um terreno {l}m x {c}m é de {a}m²')
+
+
+# largura = float(input('Largura (m): '))
+# comprimento = float(input('Comprimento (m): '))
+# area(largura, comprimento)
+
+# def escreva(msg):
+#     tam = len(msg) + 4
+#     print('~' * tam)
+#     print(f'  {msg}')
+#     print('~' * tam)
+
+
+# escreva('teste')
+# escreva('print usando função')
+
+
