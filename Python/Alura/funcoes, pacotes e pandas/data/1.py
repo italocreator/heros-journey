ee7a56d1863126716618d78a3d5f0412071539ee
Italo <76692361+italocreator@users.noreply.github.com>
2021-10-21 19:00:10 -0300

    Update 1.py

--- Python/Alura/funcoes, pacotes e pandas/data/1.py
+++ Python/Alura/funcoes, pacotes e pandas/data/1.py
@@ -1,6 +1,11 @@
 import pandas as pd
 # seta o numero de linhas para poder printar o dataframe
 pd.set_option('display.max_rows', 1000)
-dataset = pd.read_csv(r'C:\Users\souza\OneDrive\Documentos\db.csv', sep=';')
+# pd.set_option('display.max_columns', 100)
+# dataset = pd.read_csv(r'C:\Users\souza\OneDrive\Documentos\db.csv', sep=';')
+dataset = pd.read_csv(
+    r'/home/italo/Documentos/GitHub/heros-journey/Python/Alura/funcoes, pacotes e pandas/data/db.csv', sep=';')
 
-print(dataset)
+# print(dataset)
+# print(dataset.dtypes)
+print(dataset[['Quilometragem', 'Valor']].describe())
