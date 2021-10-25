ba5d3537dc5a3afcd1883fd4ae5f15934dc273ef
italocreator <contato.italoandrade@gmail.com>
2021-10-25 19:47:07 -0300

    Create FparaFatorial.py

---
+++ Python/Curso em vídeo/Exercicios_Python_m3/FparaFatorial.py
@@ -0,0 +1,12 @@
+# def fatorial(n, show=False):
+#     f = 1
+#     for c in range(n, 0, -1):
+#         if show:
+#             print(f'{c} x ')
+#         f *= c
+#     return f
+
+
+
+# #Programa Principal
+# print(fatorial(5, show=True))
