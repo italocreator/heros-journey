63d44a25bf3fdb5efae448031f5fe5bf35ba34ed
Italo <soul.italo@gmail.com>
2022-03-05 09:26:51 -0300

    Desafios Iniciais em Python

---
+++ Desafios Iniciais em Python - Cognizant Data Cloud Engineer #2/Triângulo.py
@@ -0,0 +1,36 @@
+"""
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo (soma de todos os lados) e apresente a mensagem:
+
+Perimetro = XX.X
+
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem:
+
+Area = XX.X
+
+Fórmula da área de um trapézio: AREA = ((A + B) x C) / 2
+
+Entrada
+A entrada contém três valores reais.
+
+Saída
+O resultado deve ser apresentado com uma casa decimal.
+
+
+Exemplo de Entrada	Exemplo de Saída
+6.0 4.0 2.0         Area = 10.0
+
+6.0 4.0 2.1         Perimetro = 12.1
+"""
+
+lados = [float(x) for x in input().split()]
+
+a = lados[0]
+b = lados[1]
+c = lados[2]
+soma = a+b+c
+area = ((a+b)*c)/2
+
+if a + b > c and a + c > b and b + c > a:
+    print(f"Perimetro = {soma:.1f}")
+else:
+    print(f"Area = {area:.1f}")
